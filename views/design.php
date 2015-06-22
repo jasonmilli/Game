d876edd79cca179ea0212f467f214a58ec98cb12
jasonmilli <jasonmllngtn@gmail.com>
2015-06-22 06:51:13 +0000

    testin

--- views/design.php
+++ views/design.php
@@ -20,8 +20,35 @@ ggggggggggggggggggggggggggggggggggg
 \$    .     .     .     .     .    ^
 \$  .      .      .      .      .  ^
 \$.       .       .       .       .^
-GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG
+GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG\n\n\n\n
 VUE;
 echo $view;
 $x = $y = 0;
-$res = 33;
+$res = 4;
+if (isset($argv[1])) $x = $argv[1];
+if (isset($argv[2])) $y = $argv[2];
+if (isset($argv[3])) $res = $argv[3];
+$vue = '';
+for ($i = 0; $i < pow(2,$res) + 1; $i++) {
+    $line = '$';
+    for ($j = 0; $j < pow(2,$res + 1) + 1; $j++) {
+        if ($i == 0) {
+            $line .= 'g';
+            continue;
+        }
+        if ($i == pow(2,$res - 1)) {
+            $line .= '.';
+            continue;
+        }
+        if ($i == pow(2,$res)) {
+            $line .= 'G';
+            continue;
+        }
+        if ($i < pow(2,$res - 1)) {
+            if ($j % (pow(2,$res) - $i + 1) == ($i - 1) * 2) $line .= '.';
+            else $line .= ' ';
+        }
+    }
+    $vue .= $line."^\n";
+}
+echo $vue;
